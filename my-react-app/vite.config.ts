1d6f9778c69b2343d80a58127dda10f14c0792f4
rameshgedela <grtramesh@gmail.com>
2026-06-08 14:37:48 +0530

    done

--- my-react-app/vite.config.ts
+++ my-react-app/vite.config.ts
@@ -4,7 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "My-React-App",
+  base: "/My-React-App/",
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
